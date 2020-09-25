f1d42c9d7a3aa6fde6b03a5f57411fc5314e2a5f
zaizaier <rylec23@gmail.com>
2020-09-25 21:41:05 +0200

    slider working

--- src/js/Slider.js
+++ src/js/Slider.js
@@ -1,9 +1,68 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { HashRouter, Route, Link, Switch, NavLink } from "react-router-dom";
+import Carousel from "react-bootstrap/Carousel";
 
 const Slider = () => {
-  return <h1>Slider</h1>;
+  return (
+    <>
+      <div className="carousel">
+        <Carousel>
+          <Carousel.Item>
+            <img
+              className="d-block w-100"
+              style={{ width: "700px", height: "350px" }}
+              src="https://images.pexels.com/photos/4041392/pexels-photo-4041392.jpeg?auto=compress&cs=tinysrgb&dpr=3&h=750&w=1260"
+              alt="natural cosmetics"
+            />
+            <Carousel.Caption style={{ color: "#590202" }}>
+              <h1>30% rabatu</h1>
+              <h2>Na wybrane kosmetyki firmy Pharmaceris.</h2>
+            </Carousel.Caption>
+          </Carousel.Item>
+          <Carousel.Item>
+            <img
+              className="d-block w-100"
+              style={{ width: "700px", height: "350px" }}
+              src="https://images.pexels.com/photos/51929/medications-cure-tablets-pharmacy-51929.jpeg?auto=compress&cs=tinysrgb&h=650&w=940"
+              alt="pink tablets"
+            />
+
+            <Carousel.Caption>
+              <h1>Sprawdź już dziś!</h1>
+              <h2>Ponad 6000 tysięcy produktów dostępnych w naszej aptece</h2>
+            </Carousel.Caption>
+          </Carousel.Item>
+          <Carousel.Item>
+            <img
+              style={{ width: "700px", height: "350px" }}
+              className="d-block w-100"
+              src="https://images.pexels.com/photos/2383010/pexels-photo-2383010.jpeg?auto=compress&cs=tinysrgb&dpr=3&h=650&w=940"
+              alt="green leaf with healthcare sign"
+            />
+
+            <Carousel.Caption>
+              <h1>Masz wątpliwości?</h1>
+              <h2>Napisz do Nas! Na pewno Ci pomożemy.</h2>
+            </Carousel.Caption>
+          </Carousel.Item>
+          <Carousel.Item>
+            <img
+              style={{ width: "650px", height: "350px" }}
+              className="d-block w-100"
+              src="https://images.pexels.com/photos/593451/pexels-photo-593451.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940"
+              alt="bottle with tablets"
+            />
+
+            <Carousel.Caption style={{ color: "#034c8c" }}>
+              <h2>Zrealizuj u nas swoją e-receptę!</h2>
+              <h2>Łatwy i szybki sposób, bez czekania na zamówienie.</h2>
+            </Carousel.Caption>
+          </Carousel.Item>
+        </Carousel>
+      </div>
+    </>
+  );
 };
 
 export default Slider;
