9f379a0950bbae486af27284b6de90e544e1ce0a
zaizaier <rylec23@gmail.com>
2020-10-07 19:26:19 +0200

    głowny komponent log.js gotowy

--- src/js/Log.js
+++ src/js/Log.js
@@ -1,6 +1,17 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState, useContext } from "react";
 import { HashRouter, Route, Link, Switch, NavLink } from "react-router-dom";
+import { UserContext } from "../providers/UserProvider";
+import SignIn from "../Login/SignIn";
+import SignUp from "../Login/SignUp";
+import LoggIn from "../Login/LoggIn";
+import UserProvider from "../providers/UserProvider";
+import ProfilePage from "../Login/ProfilePage";
 const Log = () => {
-  return <h1>Logowanie</h1>;
+  return (
+    <UserProvider>
+      <LoggIn />
+    </UserProvider>
+  );
 };
 export default Log;
+//ok
