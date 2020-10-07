551c6234661d5235314f73b252d4147dd47b9e57
zaizaier <rylec23@gmail.com>
2020-10-07 19:33:41 +0200

    user provider done

--- src/providers/UserProvider.jsx
+++ src/providers/UserProvider.jsx
@@ -1,30 +1,29 @@
 import React, { Component, createContext } from "react";
 import { auth, generateUserDocument } from "../firebase";
-//wartosc null to wartosc domyslna dla consumera, wykorzystywana kiedy w jego poblizu nie bedzie zadnego providera
-export const UserContext = createContext({ user: null }); //tworze kontekst--> w rzeczywistosci to obiekt kroy składa sie z 2 komponentów : provider i consumer
+
+export const UserContext = createContext({ user: null });
+
 class UserProvider extends Component {
   state = {
     user: null,
   };
 
-  // componentDidMount = () => {
-  //   auth.onAuthStateChanged((userAuth) => {
-  //     this.setState({ user: userAuth });
-  //   });
-  //   console.log(this.state.user);
-  // };
   componentDidMount = async () => {
     auth.onAuthStateChanged(async (userAuth) => {
       const user = await generateUserDocument(userAuth);
       this.setState({ user });
     });
   };
+
   render() {
+    const { user } = this.state;
+
     return (
-      <UserContext.Provider value={this.state.user}>
+      <UserContext.Provider value={user}>
         {this.props.children}
       </UserContext.Provider>
     );
   }
 }
+
 export default UserProvider;
